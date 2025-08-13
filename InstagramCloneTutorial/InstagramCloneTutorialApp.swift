8376893ce38f8a47f9bfa1a51054a1265c0f4c0a
dmakarau <denis.makarow@gmail.com>
2025-08-13 22:02:25 +0200

    Initial Commit

---
+++ InstagramCloneTutorial/InstagramCloneTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneTutorialApp.swift
+//  InstagramCloneTutorial
+//
+//  Created by Denis Makarau on 13.08.25.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
