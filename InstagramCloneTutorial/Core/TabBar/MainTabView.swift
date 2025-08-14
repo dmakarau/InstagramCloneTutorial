1f868e728db8e374efbbc190df6d3807dc737cd1
dmakarau <denis.makarow@gmail.com>
2025-08-14 06:29:14 +0200

    TabBar View setup

---
+++ InstagramCloneTutorial/Core/TabBar/MainTabView.swift
@@ -0,0 +1,40 @@
+//
+//  MainTabView.swift
+//  InstagramCloneTutorial
+//
+//  Created by Denis Makarau on 14.08.25.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            Text("Upload post")
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
+        .accentColor(.black)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
