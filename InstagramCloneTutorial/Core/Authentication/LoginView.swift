68c30aa79ecfc850747c86f3f52b7e10d8be8478
dmakarau <denis.makarow@gmail.com>
2025-08-19 14:58:14 +0200

    Implemented LoginView

--- InstagramCloneTutorial/Core/Authentication/LoginView.swift
+++ InstagramCloneTutorial/Core/Authentication/LoginView.swift
@@ -8,8 +8,110 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                
+                Spacer()
+                
+                // Logo image
+                Image("instagram")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 220, height: 100)
+
+                
+                // text fields
+                VStack {
+                    TextField("Enter your email", text: $email)
+                        .textInputAutocapitalization(.none)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .clipShape(RoundedRectangle(cornerRadius: 24))
+                        .padding(.horizontal, 24)
+                    
+                    SecureField("Enter your password", text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .clipShape(RoundedRectangle(cornerRadius: 24))
+                        .padding(.horizontal, 24)
+                }
+                
+                Button {
+                    print("Show forgot password")
+                } label: {
+                    Text("Forgot Password")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                        .padding(.trailing, 28)
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                
+                Button {
+                    print("Login")
+                } label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .frame(width: 360, height: 44)
+                        .background(Color(.systemBlue))
+                        .foregroundStyle(.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Rectangle()
+                        .frame(
+                            width: (UIScreen.main.bounds.width / 2) - 40,
+                            height: 0.5
+                        )
+                    
+                    Text("OR")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                    
+                    Rectangle()
+                        .frame(
+                            width: (UIScreen.main.bounds.width / 2) - 40,
+                            height: 0.5
+                        )
+                }
+                .foregroundStyle(.gray)
+                
+                HStack {
+                    Image("facebook_logo")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                    Text("Continue with Facebook")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(Color(.systemBlue))
+                }
+                .padding(.top, 8)
+                
+                Spacer()
+                
+                Divider()
+                
+                NavigationLink {
+                    Text("Sign up")
+                } label: {
+                    HStack (spacing: 3) {
+                        Text("Don't have an account?")
+                        Text("Sign up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                }
+                .padding(.vertical, 16)
+            }
+        }
     }
 }
 
