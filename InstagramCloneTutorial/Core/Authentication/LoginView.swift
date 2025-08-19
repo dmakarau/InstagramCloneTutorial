60710b3926c38d56eef6358bd26c9281dde71e8a
dmakarau <denis.makarow@gmail.com>
2025-08-19 14:49:15 +0200

    Moved SearchView to appropriate folder

---
+++ InstagramCloneTutorial/Core/Authentication/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  InstagramCloneTutorial
+//
+//  Created by Denis Makarau on 19.08.25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
