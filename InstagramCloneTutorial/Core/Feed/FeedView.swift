d11e8192420af48301791b6a812757c9a37f1b1d
dmakarau <denis.makarow@gmail.com>
2025-08-19 09:14:08 +0200

    Implemented FeedView

---
+++ InstagramCloneTutorial/Core/Feed/FeedView.swift
@@ -0,0 +1,40 @@
+//
+//  FeedView.swift
+//  InstagramCloneTutorial
+//
+//  Created by Denis Makarau on 19.08.25.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0 ... 10, id: \.self) { post in
+                        FeedCell()
+                    }
+                }
+                .padding(.top, 8)
+            }
+            .navigationTitle("Feed")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Image("instagram")
+                        .resizable()
+                        .frame(width: 100, height: 32)
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    FeedView()
+}
